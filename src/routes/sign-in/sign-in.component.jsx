b8166e39b4dffb52d13a9f9cc4bd6530f553431d
unknown <alisa.borkowska@gmail.com>
2025-03-10 01:59:04 +0300

    login with google poppup and redirect added

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,17 +1,38 @@
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+
 import {
+  auth,
   signInWithGooglePopup,
   createUserDocumentFromAuth,
+  signInWithGoogleRedirect,
 } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
+  useEffect(() => {
+    async function checkIn() {
+      const response = await getRedirectResult(auth);
+      if (response) {
+        const userDocRef = await createUserDocumentFromAuth(response.user);
+      }
+    }
+    checkIn();
+  }, []);
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
+  const logGoogleUserRedirect = async () => {
+    const { user } = await signInWithGoogleRedirect();
+  };
+
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Poppup</button>
+      <button onClick={logGoogleUserRedirect}>
+        Sign in with Google Redirect
+      </button>
     </div>
   );
 };

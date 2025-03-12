5b2295d930f917b45616b0d93f58eef85d1b0ea5
Alisa Borkowska <alisa.borkowska@gmail.com>
2025-03-12 18:55:51 +0100

    categories preview component

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
 import { UserProvider } from './contexts/user.context.jsx'
-import { ProductsProvider } from './contexts/products.context.jsx'
+import { CategoriesProvider } from './contexts/categories.context.jsx'
 import { CartProvider } from './contexts/cart.context.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
@@ -13,11 +13,11 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <ProductsProvider>
+        <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
-        </ProductsProvider>
+        </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>

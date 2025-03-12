c98f097af359b791ba0404c92849fef23dd3acbf
Alisa Borkowska <alisa.borkowska@gmail.com>
2025-03-12 15:57:14 +0100

    added product data to firebase database, as well as geting this data to product context component and structurized it

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,12 +1,29 @@
-import { createContext, useState } from 'react'
-import PRODUCTS from '../shop-data.json'
+import { createContext, useState, useEffect } from 'react'
+
+import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils.js'
+
+// import SHOP_DATA from '../shop-data.js'
 
 export const ProductsContext = createContext({
   products: [],
 })
 
 export const ProductsProvider = ({ children }) => {
-  const [products] = useState(PRODUCTS)
+  const [products] = useState([])
+
+  //ONCE! Added data to Firebase db
+  // useEffect(() => {
+  //   addCollectionAndDocuments('categories', SHOP_DATA)
+  // }, [])
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments()
+      console.log(categoryMap)
+    }
+    getCategoriesMap()
+  }, [])
+
   const value = { products }
   return (
     <ProductsContext.Provider value={value}>
